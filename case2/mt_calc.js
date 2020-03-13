36592a5848cd758e4f18a8fe734ca261b9a5c6e2
Ayden <amcclurg224@west-mec.org>
2020-03-13 16:32:17 -0700

    finished case 2

--- case2/mt_calc.js
+++ case2/mt_calc.js
@@ -34,7 +34,57 @@
 */
 
 
-
+window.onload = init;
+// set up the event handlers for the page
+function init(){
+   var calcButtons = document.getElementsByClassName("calcButton")
+   for(var i = 0; i < calcButtons.length; i++){
+      calcButtons[i].addEventListener("click", buttonClick);
+   }
+   document.getElementById("calcWindow").addEventListener("keydown", calcKeys);
+}
+// change what appears in the calc window when the user clicks a button
+function buttonClick(e){
+   var calcValue = document.getElementById("calcWindow").value;
+   var calcDecimal = document.getElementById("decimals").value;
+   var buttonValue = e.target.value;
+   switch(buttonValue){
+      case "del" :
+         calcValue = "";
+      break;
+      case "bksp" :
+         calcValue = eraseChar(calcValue);
+      break;
+      case "enter":
+         calcValue += " = " + evalEq(calcValue, calcDecimal) + "\n";
+      break;
+      case "prev":
+         calcValue += lastEq(calcValue);
+      break;
+      default:
+         calcValue += buttonValue;
+   }
+   document.getElementById("calcWindow").value = calcValue;
+   document.getElementById("calcWindow").focus();
+}
+// control the keyboard actions withing the calc window
+function calcKeys(e){
+   var calcValue = document.getElementById("calcWindow").value;
+   var calcDecimal = document.getElementById("decimals").value;
+   switch(e.key){
+      case "Delete":
+         calcValue = "";
+      break;
+      case "Enter":
+         calcValue += " = " + evalEq(calcValue, calcDecimal);
+      break;
+      case "ArrowUp":
+         calcValue += lastEq(calcValue);
+         e.preventDefault();
+      break;
+   }
+   document.getElementById("calcWindow").value = calcValue;
+}
 
 
 
